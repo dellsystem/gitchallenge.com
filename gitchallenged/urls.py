5ce68bb861054128a22b19259cbccf4643024782
dellsystem <ilostwaldo@gmail.com>
2014-02-15 06:49:24 -0500

    Add basic template for homepage view

--- gitchallenged/urls.py
+++ gitchallenged/urls.py
@@ -4,9 +4,6 @@ from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'gitchallenged.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
-
+    url(r'^$', 'gitchallenged.views.home', name='home'),
     url(r'^admin/', include(admin.site.urls)),
 )
